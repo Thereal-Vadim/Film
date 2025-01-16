aa40037781230ca2bd037ab519c970b258dc9cfc
Thereal-Vadim <126063869+Thereal-Vadim@users.noreply.github.com>
2025-01-16 12:45:23 +0100

    Done

---
+++ movie_project/films/urls.py
@@ -0,0 +1,8 @@
+# films/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.movie_list, name='movie_list'),
+    path('add/', views.add_movie, name='add_movie'),
+]
